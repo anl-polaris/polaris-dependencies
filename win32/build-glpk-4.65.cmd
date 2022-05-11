e65aa95bc8764e008c4a953f3a405517bef398d4
Griffin White <55763882+whiteg-anl@users.noreply.github.com>
2022-05-11 10:25:38 -0500

    Tensorflow dl (#2)
    * shuffled files around
    
    * moved build files into OS specific folders
    
    * fixed windows build
    
    * added gitattributes
    
    * fixed linux build
    
    * tensorflow downloads from GitHub
    
    * fixed issues copying tflite - need to solve permissions issue as well...

--- win32/build-glpk-4.65.cmd
+++ win32/build-glpk-4.65.cmd
@@ -7,7 +7,7 @@ IF NOT "%1" == "" (
 ) ELSE IF NOT "%POLARIS_DEPS_DIR%" == "" (
 	  set BASEDIR=%POLARIS_DEPS_DIR%
 ) ELSE (
-	set BASEDIR=c:\opt\polarisdeps\msvc-15.0
+	set BASEDIR=c:\opt\polaris\deps
 )
 
 :: get our slashes all straightened out :)
